f2b9737a4b49dc54a32186b746b5d8d5394efd51
aksilont <aksilont@gmail.com>
2021-05-05 18:23:40 +0300

    Добавлены правила .swiftlint.yml

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
