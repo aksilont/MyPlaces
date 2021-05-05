dc2884d911593fcc2ed975f9a8da0c12d8baafc9
aksilont <aksilont@gmail.com>
2021-05-05 19:26:16 +0300

    Доработка UITableViewController

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Aksilont on 05.05.2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - TableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = restaurantNames[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+
+}
