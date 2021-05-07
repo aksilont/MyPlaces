8e9e8d3cda727e24ad12ddd643de1796244641c2
aksilont <aksilont@gmail.com>
2021-05-07 14:56:30 +0300

    Image and Photo Picker View Controller

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import PhotosUI
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +23,31 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             
+            let cameraIcon = UIImage(systemName: "camera")
+            let photoIcon = UIImage(systemName: "photo")
+            
+            let actionSheet = UIAlertController(title: nil,
+                                               message: nil,
+                                               preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker()
+            }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.choosePhoto()
+            }
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true, completion: nil)
         } else {
             view.endEditing(true)
         }
@@ -36,4 +64,63 @@ extension NewPlaceViewController: UITextFieldDelegate {
         return true
     }
     
+}
+
+// MARK: - Work with image
+
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType = .camera) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        
+        dismiss(animated: true)
+    }
+    
+}
+
+// MARK: - PHPickerViewControllerDelegate
+
+extension NewPlaceViewController: PHPickerViewControllerDelegate {
+    
+    func choosePhoto() {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+
+        let pickerViewController = PHPickerViewController(configuration: config)
+        pickerViewController.delegate = self
+        self.present(pickerViewController, animated: true, completion: nil)
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true, completion: nil)
+        
+        let itemProviders = results.map(\.itemProvider)
+        
+        itemProviders.forEach { item in
+            if item.canLoadObject(ofClass: UIImage.self) {
+                item.loadObject(ofClass: UIImage.self) { object, _ in
+                    DispatchQueue.main.async {
+                        if let image = object as? UIImage {
+                            self.imageOfPlace.image = image
+                            self.imageOfPlace.contentMode = .scaleAspectFill
+                            self.imageOfPlace.clipsToBounds = true
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
