c86a4624a8b9dcf1532c57586cbaff6d7004c27f
aksilont <aksilont@gmail.com>
2021-05-10 02:51:10 +0300

    Добавлен поиск

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import RealmSwift
 
-class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView! {
         didSet {
@@ -20,59 +20,31 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var segmentedConrol: UISegmentedControl!
     @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
     
-    var places: Results<Place>!
-    var ascendingSorting = true
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var places: Results<Place>!
+    private var filteredPlaces: Results<Place>!
+    private var ascendingSorting = true
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return true }
+        return text.isEmpty
+    }
+    private var isFiltireing: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         places = realm.objects(Place.self)
         sorting(reloadTableView: false)
-    }
-
-    // MARK: - TableViewDataSource
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.isEmpty ? 0 : places.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? CustomTableViewCell
-        else { return UITableViewCell() }
-
-        let place = places[indexPath.row]
-
-        cell.nameLabel.text = place.name
-        cell.locationLabel.text = place.location
-        cell.typeLabel.text = place.type
-        cell.imageOfPlace.image = UIImage(data: place.imageData!)
-        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
-        cell.imageOfPlace.clipsToBounds = true
-
-        return cell
-    }
-    
-    // MARK: - UITableViewDelegate
-    
-    func tableView(_ tableView: UITableView,
-                   trailingSwipeActionsConfigurationForRowAt
-                    indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let place = places[indexPath.row]
         
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _,_,_ in
-            StorageManager.deleteObject(place)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-        }
-        let configAction = UISwipeActionsConfiguration(actions: [deleteAction])
-        return configAction
-    }
-    
-    // MARK: - Unwind Segue
-    
-    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
-        guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
-        newPlaceVC.savePlace()
-        tableView.reloadData()
+        // Setup the search controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        definesPresentationContext = true
     }
     
     // MARK: - Navigation
@@ -82,11 +54,19 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
             guard let indexPath = tableView.indexPathForSelectedRow,
                   let newPlaceVC = segue.destination as? NewPlaceViewController
             else { return }
-            let place = places[indexPath.row]
+            
+            var place = Place()
+            if isFiltireing {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             newPlaceVC.currentPlace = place
         }
     }
-
+    
+    // MARK: - Sorting
+    
     private func sorting(reloadTableView: Bool = true) {
         switch segmentedConrol.selectedSegmentIndex {
         case 0:
@@ -98,6 +78,16 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
         if reloadTableView { tableView.reloadData() }
     }
+    
+    // MARK: - Unwind Segue
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
+        newPlaceVC.savePlace()
+        tableView.reloadData()
+    }
+    
+    // MARK: - IBActions
     
     @IBAction func sortSelectection(_ sender: UISegmentedControl) {
         sorting()
@@ -113,4 +103,76 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         sorting()
     }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - Table view Data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltireing {
+            return filteredPlaces.count
+        }
+        return places.isEmpty ? 0 : places.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? CustomTableViewCell
+        else { return UITableViewCell() }
+
+        var place = Place()
+        if isFiltireing {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
+
+        cell.nameLabel.text = place.name
+        cell.locationLabel.text = place.location
+        cell.typeLabel.text = place.type
+        cell.imageOfPlace.image = UIImage(data: place.imageData!)
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.clipsToBounds = true
+
+        return cell
+    }
+    
+    // MARK: - Table view Delegate
+    
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt
+                    indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        var place = Place()
+        if isFiltireing {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _,_,_ in
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        let configAction = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configAction
+    }
+    
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension MainViewController: UISearchResultsUpdating {
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText)
+        tableView.reloadData()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
 }
